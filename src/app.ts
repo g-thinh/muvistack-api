b0151a1263112619f0cf49daabd22f45d192a722
Gia Thinh Nguyen <gthinh.nguyen.dev@gmail.com>
2023-12-28 18:12:02 -0500

    add basic express middlewares and route

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { Application } from "express";
+import helloRoutes from "./routes/helloRoutes";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import logger from "./utils/logger";
+import expressPino from "pino-http";
+
+dotenv.config();
+
+const expressLogger = expressPino({ logger });
+
+const configureMiddlewares = (app: Application) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(helmet());
+  app.use(expressLogger);
+};
+
+const configureRoutes = (app: Application) => {
+  app.use("/", helloRoutes);
+};
+
+const configureApp = (): Application => {
+  const app = express();
+
+  configureMiddlewares(app);
+  configureRoutes(app);
+
+  logger.info("App configured...");
+
+  return app;
+};
+
+export default configureApp();
